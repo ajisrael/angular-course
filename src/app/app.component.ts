c50d9b545d8f1cbb83ce86334b27244f7315ee8a
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2025-02-06 07:57:28 -0500

    f   - added course card component and fixed standalone module issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { COURSES } from '../db-data';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CourseCardComponent]
 })
 export class AppComponent {
 
